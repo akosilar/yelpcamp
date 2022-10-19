7c457be932b355628c3d6a1c1c8398f9ff7f7199
Lars Dev <janlarsaustero@gmail.com>
2022-10-19 00:08:51 -0400

    test title test commit tag test

--- js/app.js
+++ js/app.js
@@ -11,29 +11,31 @@
 // console.log("after conditional");
 
 
-// const dayOfWeek = 'Friday';
+const dayOfWeek = prompt('enter a day').toLowerCase();
 
-// if (dayOfWeek === 'Monday') {
-//     console.log("i hate mondays");
-// }
-// else if (dayOfWeek === 'Saturday') {
-//     console.log("i love saturdays");
-// }
-// else if (dayOfWeek === 'Friday') {
-//     console.log("fridays are decent especially after work!");
-// }
+if (dayOfWeek === 'monday') {
+    console.log("i hate mondays");
+}
+else if (dayOfWeek === 'saturday') {
+    console.log("i love saturdays");
+}
+else if (dayOfWeek === 'friday') {
+    console.log("fridays are decent especially after work!");
+}
+else {
+    console.log("meh");
+}
+// const age = 66;
 
-const age = 66;
-
-if (age <= 5) {
-    console.log(free);
-}
-else if (age <10) {
-    console.log("child $10");
-}
-else if (age <65) {
-    console.log("adult $20");
-}
-else if (age >=65) {
-    console.log("boomer $10");
-}
+// if (age <= 5) {
+//     console.log(free);
+// }
+// else if (age <10) {
+//     console.log("child $10");
+// }
+// else if (age <65) {
+//     console.log("adult $20");
+// }
+// else if (age >=65) {
+//     console.log("boomer $10");
+// }
