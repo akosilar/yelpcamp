245db3dd6fdb71d6a52e58f1409d86c0f83941c0
Lars Dev <janlarsaustero@gmail.com>
2022-10-18 23:40:28 -0400

    more conditionals

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
-console.log("it's working")
+// console.log("before conditional");
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("your number is less than 0.5!!");
+//     console.log(random);
+// }
+// else{
+//     console.log("your number is greater than 0.5!!");
+//     console.log(random);
+// }
+// console.log("after conditional");
+
+
+// const dayOfWeek = 'Friday';
+
+// if (dayOfWeek === 'Monday') {
+//     console.log("i hate mondays");
+// }
+// else if (dayOfWeek === 'Saturday') {
+//     console.log("i love saturdays");
+// }
+// else if (dayOfWeek === 'Friday') {
+//     console.log("fridays are decent especially after work!");
+// }
+
+const age = 66;
+
+if (age <= 5) {
+    console.log(free);
+}
+else if (age <10) {
+    console.log("child $10");
+}
+else if (age <65) {
+    console.log("adult $20");
+}
+else if (age >=65) {
+    console.log("boomer $10");
+}
