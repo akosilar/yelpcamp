c3764a763f17cb45c99e0c1377f5902be36cfcc3
Lars Dev <janlarsaustero@gmail.com>
2022-10-27 01:13:20 -0400

    starting functions and arguments

---
+++ js/functions/app.js
@@ -0,0 +1,13 @@
+function singSong() {
+    console.log('do');
+    console.log('re');
+    console.log('mi');
+}
+
+singSong();
+
+function greet(person) {
+    console.log(`hi, ${person}`);
+}
+
+greet(prompt('name'));
