beac7ceddf2c55c7b527c2b1fb41f2c503adde87
Lars Dev <janlarsaustero@gmail.com>
2022-10-28 21:53:36 -0400

    on this in functions

--- js/functions/app.js
+++ js/functions/app.js
@@ -10,4 +10,51 @@ function greet(person) {
     console.log(`hi, ${person}`);
 }
 
-greet(prompt('name'));
+// greet(prompt('name'));
+const week = {
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+    7: 'Sunday'
+}
+let num = 2;
+console.log(week[num]);
+
+const player = {
+    name: 'lars',
+    rating: 1500
+}
+
+console.log(`${player.name} is rated ${player.rating}`);
+
+function returnDay(num) {
+    const week = {
+         1: 'Monday',
+         2: 'Tuesday',
+         3: 'Wednesday',
+         4: 'Thursday',
+         5: 'Friday',
+         6: 'Saturday',
+         7: 'Sunday'
+     }
+     if(num >= 1 && num <= 7){
+         return week[num];
+     }
+     else {
+         return null;
+     }
+ }
+console.log(returnDay(5));
+
+const cat = {
+    name: 'maymay',
+    color: 'orange',
+    meow() {
+        console.log("meow meow");
+        console.log(`${this.name}`);
+    }
+    
+}
