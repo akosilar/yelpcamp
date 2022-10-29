e098b9761acf70f367c39972ea9e74c9ad71974e
akosilar <55524614+akosilar@users.noreply.github.com>
2022-10-29 18:26:02 -0400

    Update app.js

--- js/functions/app.js
+++ js/functions/app.js
@@ -11,3 +11,24 @@ function greet(person) {
 }
 
 greet(prompt('name'));
+
+function repeat(message, reps) {
+    for(let i = 1; i <= reps; i++) {
+        console.log(message);
+    }
+}
+
+repeat('hola', 3);
+
+function add(x,y) {
+    a = parseInt(x);
+    b = parseInt(y);
+    if(typeof a !== 'number' || typeof b !== 'number'){
+        console.log('enter a number!');
+    }
+    else {
+        return a+b;
+    }
+}
+
+console.log(add(prompt('enter x'),prompt('enter y')));
