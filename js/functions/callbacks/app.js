a745b21bdfc60b147e9d8b6fcaf8053ea5b220b2
Lars Dev <janlarsaustero@gmail.com>
2022-10-30 12:42:01 -0400

    finished callbacks
    ended with arrow function behavior with keyword this

--- js/functions/callbacks/app.js
+++ js/functions/callbacks/app.js
@@ -56,6 +56,68 @@ setTimeout(() => {
     console.log('hello after 3 secs')
 }, 3000);
 
-const id = setInterval(() => {
-    console.log(`${Math.random()}`);
-},1000);
+// const id = setInterval(() => {
+//     console.log(`${Math.random()}`);
+// },1000);
+
+let usernames = ['mark', 'staceysmom1978', 'q29832128238983', 'carrie98', 'MoanaFan'];
+
+function validUsernames(usernames) {
+    return usernames.filter(un => usernames.length < 10);
+}
+
+const exams = [80,98,92,78,7,90,89,84,81,77];
+
+exams.every(score => score>= 75);
+exams.some(score => score>= 75);
+
+const prices =[9.99,1.50,19.99,49.99,30.50];
+// const total = prices.reduce((total, price) => {
+//     return total + price;
+// })
+
+const total = prices.reduce((total, price) =>  total + price
+);
+
+const minPrice = prices.reduce((min,price) => {
+    if (price<min){
+        return price;
+    }
+    else {
+        return min;
+    }
+});
+
+const maxPrice = prices.reduce((max,price) => {
+    if (price>max){
+        return price;
+    }
+    else {
+        return max;
+    }
+});
+
+const highestRated = movies.reduce((bestMovie,currMovie) => {
+    if(currMovie.score > bestMovie.score) {
+        return currMovie;
+    }else {
+        return bestMovie;
+    }
+});
+
+const evens = [2,4,6,8];
+const evenSum = evens.reduce((sum,num) => sum + num, 100); //has an initial value of 100
+
+const person = {
+    firstName: 'vertigo',
+    lastName: 'mortensen',
+    fullName: function () {
+        return `${this.firstName}  ${this.lastName}`
+    },
+    shoutName: function () {
+        setTimeout(() => {
+            console.log(this);
+            console.log(person.fullName());
+        },3000)
+    }
+}
