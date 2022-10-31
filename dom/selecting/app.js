a6235517f5fed7a4b2a9d92a83b7c661513edc50
akosilar <55524614+akosilar@users.noreply.github.com>
2022-10-31 06:53:27 -0400

    on attributes

--- dom/selecting/app.js
+++ dom/selecting/app.js
@@ -10,15 +10,15 @@ for (let img of squareImages) {
     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
 }
 
-// const squareImages = document.getElementsByClassName('square');
+const links = document.querySelectorAll('p a');
 
-// for (let img of squareImages) {
-//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
-// }
+for (let link of links) {
+    console.log(link.href);
+}
 
-// const links = document.querySelectorAll('p a');
+const allLinks = document.querySelectorAll('a');
 
-// for (let link of links) {
-//     console.log(link.href)
-// }
+for(let link of allLinks) {
+    link.innerText = 'i am a link';
+}
 
