7f77daa3d9c2a792d5b481a44b3cedbfad002b4e
akosilar <55524614+akosilar@users.noreply.github.com>
2022-11-01 23:33:26 -0400

    after pokemon

--- dom/selecting/pokemon/app.js
+++ dom/selecting/pokemon/app.js
@@ -1,5 +1,16 @@
 
 const container = document.querySelector('#container');
-const newImg = document.createElement('img');
-newImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png';
-container.appendChild(newImg);
+
+
+
+for(let i = 1; i<255; i++) {
+    const newImg = document.createElement('img');
+    newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon')
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
+}
