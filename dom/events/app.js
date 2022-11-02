1f42c1f5ab602df19f9451ada9c6a3b9e5ca00ec
akosilar <55524614+akosilar@users.noreply.github.com>
2022-11-02 06:52:45 -0400

    on addventlisteners

--- dom/events/app.js
+++ dom/events/app.js
@@ -3,3 +3,21 @@ btn.onclick = function () {
     alert('button 2 clicked');
 
 }
+
+function scream () {
+    alert('how dare you enter my range!');
+}
+
+function inform () {
+    alert('button 3 clicked');
+}
+
+btn.onmouseenter = scream;
+
+document.querySelector('h1').onclick = () => {
+    alert('you clicked on an h1');
+};
+
+
+const btn3 = document.querySelector('#v3');
+btn3.addEventListener('click', inform);
