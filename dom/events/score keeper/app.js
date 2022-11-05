e47aa70e369b77e4d5d23019f0596c7915245a01
Lars Dev <janlarsaustero@gmail.com>
2022-11-05 18:37:07 -0400

    refactored score keeper

--- dom/events/score keeper/app.js
+++ dom/events/score keeper/app.js
@@ -1,3 +1,13 @@
+const p1 = {
+    button: document.querySelector('#increase1'),
+    display: document.querySelector('#score1')
+}
+
+const p2 = {
+    button: document.querySelector('#increase2'),
+    display: document.querySelector('#score2')
+}
+
 const scoreOne = document.querySelector('#score1');
 const scoreTwo = document.querySelector('#score2');
 
@@ -7,35 +17,49 @@ const button2 = document.querySelector('#increase2');
 
 const limit = document.querySelector('#scoreLimit');
 
-button1.addEventListener('click', function(){
-    let score = parseInt(scoreOne.innerText);
+function updateScores(player, opponent) {
+    let score = parseInt(player.display.innerText);
     score ++;
-    scoreOne.innerText = score;
-    console.log(scoreOne);
-    console.log(limit.value);
-    if(score === parseInt(limit.value)) {
-        button1.setAttribute('disabled', true);
-        button2.setAttribute('disabled', true);
-        score1.classList.add('has-text-success');
-        score2.classList.add('has-text-danger');
-
-
+    player.display.innerText = score;
+    if(score === parseInt(limit.value)){
+        player.display.classList.add('has-text-success');
+        opponent.display.classList.add('has-text-danger');
+        player.button.setAttribute('disabled', true);
+        opponent.button.setAttribute('disabled',true);
     }
+}
+
+p1.button.addEventListener('click', function(){
+    updateScores(p1,p2);
+    // let score = parseInt(scoreOne.innerText);
+    // score ++;
+    // scoreOne.innerText = score;
+    // console.log(scoreOne);
+    // console.log(limit.value);
+    // if(score === parseInt(limit.value)) {
+    //     button1.setAttribute('disabled', true);
+    //     button2.setAttribute('disabled', true);
+    //     score1.classList.add('has-text-success');
+    //     score2.classList.add('has-text-danger');
+
+
+    // }
 })
 
-button2.addEventListener('click', function(){
-    let score = parseInt(scoreTwo.innerText);
-    score ++;
-    scoreTwo.innerText = score;
-    console.log(scoreTwo);
-    console.log(limit.value);
-    if(score === parseInt(limit.value)) {
-        button1.setAttribute('disabled', true);
+p2.button.addEventListener('click', function(){
+    updateScores(p2,p1);
+    // let score = parseInt(scoreTwo.innerText);
+    // score ++;
+    // scoreTwo.innerText = score;
+    // console.log(scoreTwo);
+    // console.log(limit.value);
+    // if(score === parseInt(limit.value)) {
+    //     button1.setAttribute('disabled', true);
 
-        button2.setAttribute('disabled', true);
-        score2.classList.add('has-text-success');
-        score1.classList.add('has-text-danger');
-    }
+    //     button2.setAttribute('disabled', true);
+    //     score2.classList.add('has-text-success');
+    //     score1.classList.add('has-text-danger');
+    // }
 })
 
 buttonReset.addEventListener('click',reset);
@@ -43,11 +67,17 @@ buttonReset.addEventListener('click',reset);
 limit.addEventListener('change', reset);
 
 function reset() {
-    scoreOne.innerText = '0';
-    scoreTwo.innerText = '0';
-    button1.removeAttribute('disabled');
-    button2.removeAttribute('disabled');
-    score2.classList.remove('has-text-success','has-text-danger');
-    score1.classList.remove('has-text-success','has-text-danger');
+    for (let p of [p1,p2]){
+        p.display.innerText = '0';
+        p.button.removeAttribute('disabled');
+        p.display.classList.remove('has-text-success','has-text-danger');
+    }
+    // p1.display.innerText = '0';
+    // p2.display.innerText = '0';
+    // p1.button.removeAttribute('disabled');
+    // p2.button.removeAttribute('disabled');
+    // p1.display.classList.remove('has-text-success','has-text-danger');
+    // p2.display.classList.remove('has-text-success','has-text-danger');
 
 }
+
