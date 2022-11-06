42232c9a3bead8fa67659d71e35b5e56c520cb42
akosilar <55524614+akosilar@users.noreply.github.com>
2022-11-06 15:55:21 -0500

    rainbow color change to promise

--- js/async/callback/app.js
+++ js/async/callback/app.js
@@ -21,16 +21,42 @@
 // }, 1000);
 
 
-const delayedColorchange = (newColor, delay, doNext) => {
-    setTimeout(()=> {
-        document.body.style.backgroundColor = newColor
-        doNext && doNext();
-    },delay)
+// const delayedColorchange = (newColor, delay, doNext) => {
+//     setTimeout(()=> {
+//         document.body.style.backgroundColor = newColor
+//         doNext && doNext();
+//     },delay)
 
+// }
+
+
+const delayedColorchange = (color,delay) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        },delay)
+    })
 }
 
-delayedColorchange('red',1000, ()=>{
-    delayedColorchange('orange', 1000, () => {
-        delayedColorchange('yellow', 1000)
+delayedColorchange('red',1000)
+    .then(() => {
+        return delayedColorchange('orange',1000)
     })
-});
+    .then(() => {
+       return delayedColorchange('yellow', 1000)
+    })
+    .then(() => delayedColorchange('green', 1000))
+    .then(() => delayedColorchange('blue', 1000))
+    .then(() => delayedColorchange('indigo', 1000))
+    .then(() => delayedColorchange('violet', 1000))
+
+
+
+
+
+// delayedColorchange('red',1000, ()=>{
+//     delayedColorchange('orange', 1000, () => {
+//         delayedColorchange('yellow', 1000)
+//     })
+// });
