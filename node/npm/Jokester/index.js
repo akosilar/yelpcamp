745326e2a9f7d32e76cd27cd349232d354bb25ec
akosilar <55524614+akosilar@users.noreply.github.com>
2022-11-13 19:26:12 -0500

    figure dadjoke
    some methods are broken

--- node/npm/Jokester/index.js
+++ node/npm/Jokester/index.js
@@ -1,6 +1,16 @@
 const jokes = require('give-me-a-joke');
 // console.dir(jokes);
 
-jokes.getRandomCNJoke(function(joke) {
-    console.log(joke);
+const colors = require('colors');
+
+jokes.getRandomDadJoke(function(joke) {
+    console.log(joke.rainbow);
 })
+// const giveMeAJoke = require('give-me-a-joke');
+
+// // To get a random dad joke
+// giveMeAJoke.getRandomDadJoke(function (joke) {
+//   console.log(joke);
+// });
+
+
