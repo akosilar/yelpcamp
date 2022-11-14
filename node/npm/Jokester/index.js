c76cfe84594d6a2a287f4fa13dc63824ae11468a
Lars Dev <janlarsaustero@gmail.com>
2022-11-13 20:12:05 -0500

    went over npm install global

--- node/npm/Jokester/index.js
+++ node/npm/Jokester/index.js
@@ -2,10 +2,13 @@ const jokes = require('give-me-a-joke');
 // console.dir(jokes);
 
 const colors = require('colors');
+const cowsay = require('cowsay');
 
 jokes.getRandomDadJoke(function(joke) {
     console.log(joke.rainbow);
 })
+
+
 // const giveMeAJoke = require('give-me-a-joke');
 
 // // To get a random dad joke
