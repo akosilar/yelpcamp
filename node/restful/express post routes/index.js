a2ccf8ed35d74fd55b365a75a444e549078e1b80
akosilar <55524614+akosilar@users.noreply.github.com>
2022-11-16 06:27:43 -0500

    added comment details, and uuid functionality

--- node/restful/express post routes/index.js
+++ node/restful/express post routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+
 
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(express.json()) // for parsing application/json
@@ -9,18 +11,22 @@ app.set('view engine', 'ejs')
 
 const comments = [
     {
+        id: uuidv4(),
         username: 'todd',
         comment: 'lmao so funny'
     },
     {
+        id: uuidv4(),    
         username: 'skyler',
         comment: 'why u mad?'
     },
     {
+        id: uuidv4(),
         username: 'art33zy',
         comment: 'gg noob'
     },
     {
+        id: uuidv4(),
         username: 'luke',
         comment: 'what a simp'
     }
@@ -39,13 +45,21 @@ app.get('/comments/new', (req,res) => {
 app.post('/comments', (req,res) => {
     const {username, comment} = req.body
     console.log(req.body)
-    comments.push({username,comment})
-    res.send('it worked')
+    comments.push({username,comment, id: uuidv4()})
+    // res.send('it worked')
+    res.redirect('/comments')
+})
+
+app.get('/comments/:id', (req,res) => {
+    const {id} = req.params
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/commentDetail', {comment})
 })
 
 app.get('/tacos', (req,res) => {
     res.send('get /tacos response')
 })
+
 
 app.post('/tacos', (req,res) => {
     console.log(req.body)
