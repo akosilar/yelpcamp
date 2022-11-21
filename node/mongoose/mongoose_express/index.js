a01b6101f6337a2877f969684d6f1f7d3f21e218
akosilar <55524614+akosilar@users.noreply.github.com>
2022-11-21 05:38:57 -0500

    set up mongo connection and basic route test

--- node/mongoose/mongoose_express/index.js
+++ node/mongoose/mongoose_express/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const app = express ();
 const path = require('path')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/movieApp')
+.then(() => {
+    console.log('mongo connection open')
+})
+.catch(err => {
+    console.log('oh noes. Mongo connection problem')
+    console.log(err)
+})
+
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+
+app.get('/dog', (req,res) => {
+    res.send('woof')
+})
 
 app.listen(3000, () => {
     console.log("app is listening on 3000")
