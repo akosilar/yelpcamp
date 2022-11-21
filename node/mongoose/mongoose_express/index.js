b99b13724ff5c34993ed3e92ae37ad0c3ac53606
akosilar <55524614+akosilar@users.noreply.github.com>
2022-11-21 06:47:05 -0500

    listed all products in the product page
    product route set up to list all products in the db

--- node/mongoose/mongoose_express/index.js
+++ node/mongoose/mongoose_express/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express ();
 const path = require('path')
 const mongoose = require('mongoose')
+const Product = require('./models/product')
 
-mongoose.connect('mongodb://localhost:27017/movieApp')
+mongoose.connect('mongodb://localhost:27017/farmStand')
 .then(() => {
     console.log('mongo connection open')
 })
@@ -16,10 +17,13 @@ mongoose.connect('mongodb://localhost:27017/movieApp')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/dog', (req,res) => {
-    res.send('woof')
+app.get('/products', async (req,res) => {
+    const products = await Product.find({})
+    console.log(products)
+    res.render('./products/index', {products})
 })
 
 app.listen(3000, () => {
     console.log("app is listening on 3000")
 })
+
