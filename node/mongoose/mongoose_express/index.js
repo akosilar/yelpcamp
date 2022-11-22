5f9234e97c61e8b6e2a27518cfe40dfe298590bc
Lars Dev <janlarsaustero@gmail.com>
2022-11-21 22:10:02 -0500

    added functionality to add products to the db

--- node/mongoose/mongoose_express/index.js
+++ node/mongoose/mongoose_express/index.js
@@ -16,19 +16,36 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
 
 //display all products
 app.get('/products', async (req,res) => {
     const products = await Product.find({})
-    console.log(products)
+    // console.log(products)
     res.render('./products/index', {products})
 })
 
+//create a product
+app.get('/products/new', (req,res) => {
+    res.render('./products/new')
+})
+
+//show product detail
 app.get('/products/:id', async (req,res) => {
     const {id} = req.params
     const product = await Product.findById(id)
     res.render('./products/detail', {product})
     console.log(product)
+})
+
+app.post('/products', async (req,res) => {
+    const {name, price, category} = req.body
+    const newProduct = new Product({name, price, category})
+    await newProduct.save()
+    console.log(name, price, category)
+
+    res.redirect(`/products/${newProduct.id}`)
 })
 
 app.listen(3000, () => {
