457bfa4b84d281f5b20c26718de62a7fe5b36d85
Lars Dev <janlarsaustero@gmail.com>
2022-11-21 23:06:39 -0500

    added functionality to edit a product
    this uses PUT request instead of PATCH

--- node/mongoose/mongoose_express/index.js
+++ node/mongoose/mongoose_express/index.js
@@ -3,6 +3,8 @@ const app = express ();
 const path = require('path')
 const mongoose = require('mongoose')
 const Product = require('./models/product')
+const methodOverride = require('method-override')
+
 
 mongoose.connect('mongodb://localhost:27017/farmStand')
 .then(() => {
@@ -17,6 +19,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(methodOverride('_method'))
 
 
 //display all products
@@ -26,7 +29,7 @@ app.get('/products', async (req,res) => {
     res.render('./products/index', {products})
 })
 
-//create a product
+//show create a product
 app.get('/products/new', (req,res) => {
     res.render('./products/new')
 })
@@ -39,13 +42,30 @@ app.get('/products/:id', async (req,res) => {
     console.log(product)
 })
 
+//show edit product
+app.get('/products/:id/edit', async (req,res) => {
+    const {id} = req.params
+    const product = await Product.findById(id)
+    res.render('./products/edit', {product})
+    console.log(product)
+})
+
+//add the new product to the db
 app.post('/products', async (req,res) => {
     const {name, price, category} = req.body
     const newProduct = new Product({name, price, category})
     await newProduct.save()
     console.log(name, price, category)
-
     res.redirect(`/products/${newProduct.id}`)
+})
+
+//edit the product in the db
+app.put('/products/:id', async (req,res) => {
+    // console.log(req.body)
+    const {id} = req.params
+    const {name, price, category} = req.body
+    const productUpdate = await Product.findByIdAndUpdate(id, {name,price,category}, {runValidators: true, new: true})
+    res.redirect(`/products/${productUpdate._id}`)
 })
 
 app.listen(3000, () => {
