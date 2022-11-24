06409ed8dc93990e4eb5f8aa3ec6be0024250319
Lars Dev <janlarsaustero@gmail.com>
2022-11-23 22:54:45 -0500

    added campgrounds page
    lists all campgrounds

--- exercises/yelpcamp/app.js
+++ exercises/yelpcamp/app.js
@@ -19,19 +19,14 @@ db.once('open', () => {
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req,res) => {
-    res.render('home')
+app.get('/campgrounds', async (req,res) => {
+   const campgrounds = await Campground.find({})
+   res.render('campgrounds/index',{campgrounds})
 })
 
-app.get('/makecampground', async (req,res) => {
-    const camp = new Campground({
-        title: 'My Backyard',
-        description: 'cheap camping'
-    })
-    await camp.save()
-    res.send(camp)
-})
+
 
 app.listen(3000, () => {
     console.log('listening on port 3000')
 })
+
