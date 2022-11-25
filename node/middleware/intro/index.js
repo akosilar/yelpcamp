ec21d2bd337ea0c60a3438c29d11d778c1851728
Lars Dev <janlarsaustero@gmail.com>
2022-11-25 11:50:58 -0500

    added 404 route middleware

--- node/middleware/intro/index.js
+++ node/middleware/intro/index.js
@@ -5,12 +5,30 @@ const morgan = require('morgan')
 
 app.use(morgan('common'))
 
+app.use((req,res,next) => {
+    req.requestTime = Date.now()
+    // console.log(req.method, req.path) 
+    next()
+})
+
+app.use('/dogs', (req,res,next) => {
+    console.log('i luv dogs')
+    next()
+})
+
 app.get('/', (req,res) => {
+    console.log(`request date: ${req.requestTime}`)
     res.send('home page')
 })
 
 app.get('/dogs', (req,res) => {
+    console.log(`request date: ${req.requestTime}`)
+
     res.send('woof woof')
+})
+
+app.use((req,res) => {
+    res.status(404).send('not found')
 })
 
 app.listen(3000, () => {
