fee00af84887869ca7003785f49763d901c04b6b
akosilar <55524614+akosilar@users.noreply.github.com>
2022-11-30 05:47:03 -0500

    added catchasync error handling

--- exercises/yelpcamp/app.js
+++ exercises/yelpcamp/app.js
@@ -11,6 +11,7 @@ const methodOverride = require('method-override')
 const app = express()
 const path = require('path')
 const ejsMate = require('ejs-mate')
+const catchAsync = require('./utils/catchAsync')
 const Campground = require('./models/campground')
 require('dotenv').config();
 
@@ -55,35 +56,41 @@ app.get('/campgrounds/new', (req,res) => {
     res.render('campgrounds/new')
 })
 //add the new campground to db
-app.post('/campgrounds', async (req,res) => {
+app.post('/campgrounds', catchAsync(async (req,res,next) => {
+
     const campground = new Campground(req.body.campground)
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
-})
+
+}))
 
 //show campground detail
-app.get('/campgrounds/:id', async (req,res) => {
+app.get('/campgrounds/:id', catchAsync(async (req,res) => {
     const search = await Campground.findById(req.params.id)
     res.render('campgrounds/show',{search})
-})
+}))
 
 //show edit campground
-app.get('/campgrounds/:id/edit', async(req,res) => {
+app.get('/campgrounds/:id/edit',catchAsync(async(req,res) => {
     const search = await Campground.findById(req.params.id)
     res.render('campgrounds/edit',{search})
-})
+}))
 //submit the campground edit to db
-app.put('/campgrounds/:id', async(req,res) => {
+app.put('/campgrounds/:id', catchAsync(async(req,res) => {
     const {id} = req.params
     const update = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${update._id}`)
-})
+}))
 
 //delete the campground from db
-app.delete('/campgrounds/:id', async(req,res) => {
+app.delete('/campgrounds/:id', catchAsync(async(req,res) => {
     const {id} = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+}))
+
+app.use((err,req,res,next) => {
+    res.send('something went wrong')
 })
 
 app.listen(3000, () => {
