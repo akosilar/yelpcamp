da047691e8f5de7a53021b7af494ce8ad7f7b73b
akosilar <55524614+akosilar@users.noreply.github.com>
2022-12-03 06:38:16 -0500

    intro to mongo relationships
    added farm ejs and js

--- node/mongoose/mongoose_express with handling async errors/index.js
+++ node/mongoose/mongoose_express with handling async errors/index.js
@@ -2,11 +2,15 @@ const express = require('express')
 const app = express ();
 const path = require('path')
 const mongoose = require('mongoose')
+
+
 const Product = require('./models/product')
+const Farm = require('./models/farm')
+
 const methodOverride = require('method-override')
 const AppError = require('./appError')
 
-mongoose.connect('mongodb://localhost:27017/farmStand')
+mongoose.connect('mongodb://localhost:27017/farmStandTake2')
 .then(() => {
     console.log('mongo connection open')
 })
@@ -22,6 +26,26 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(methodOverride('_method'))
+
+//farm routes
+app.get('/farms', async(req,res) => {
+    const farms = await Farm.find({})
+    res.render('farms/index', {farms})
+})
+
+app.get('/farms/new', (req,res) => {
+    res.render('farms/new')
+})
+
+app.post('/farms', async (req,res) => {
+    const farm = new Farm(req.body)
+    await farm.save()
+    res.redirect('/farms')
+})
+
+
+//product routes
+
 
 const categories = ['fruit', 'vegetable', 'dairy', 'mushrooms']
 
