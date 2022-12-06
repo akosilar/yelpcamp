125ef47e3f5dd9595e3615a7446e1f1ff7e118a2
akosilar <55524614+akosilar@users.noreply.github.com>
2022-12-06 07:33:53 -0500

    added post route for reviews

--- exercises/yelpcamp/app.js
+++ exercises/yelpcamp/app.js
@@ -14,6 +14,7 @@ const ejsMate = require('ejs-mate')
 const catchAsync = require('./utils/catchAsync')
 const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campground')
+const Review = require('./models/review')
 const {campgroundSchema} = require('./schemas.js')
 require('dotenv').config();
 
@@ -103,6 +104,15 @@ app.delete('/campgrounds/:id', catchAsync(async(req,res) => {
     const {id} = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async(req,res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    campground.reviews.push(review)
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.all('*', (req,res,next) => {
