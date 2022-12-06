062a18a05b7320ed02d490358128ed4f18cded32
akosilar <55524614+akosilar@users.noreply.github.com>
2022-12-06 09:38:44 -0500

    added delete middleware
    deletes reviews along with the associated campground

--- exercises/yelpcamp/app.js
+++ exercises/yelpcamp/app.js
@@ -95,7 +95,7 @@ app.post('/campgrounds', validateCampground,catchAsync(async (req,res,next) => {
 
 //show campground detail
 app.get('/campgrounds/:id', catchAsync(async (req,res) => {
-    const search = await Campground.findById(req.params.id)
+    const search = await Campground.findById(req.params.id).populate('reviews')
     res.render('campgrounds/show',{search})
 }))
 
@@ -125,6 +125,13 @@ app.post('/campgrounds/:id/reviews',validateReview, catchAsync(async(req,res) =>
     await review.save()
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async(req,res) => {
+    const {id, reviewId} = req.params
+    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`)
 }))
 
 app.all('*', (req,res,next) => {
