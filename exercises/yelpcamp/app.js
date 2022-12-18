df149a47c1b0b3c82b4103a6bb567c12e007f962
akosilar <55524614+akosilar@users.noreply.github.com>
2022-12-18 18:07:36 -0500

    intro to express router

--- exercises/yelpcamp/app.js
+++ exercises/yelpcamp/app.js
@@ -1,10 +1,3 @@
-// import { createRequire } from "module";
-// import nodeFetch from 'node-fetch';
-
-// const require = createRequire(import.meta.url);
-// import { createApi } from 'unsplash-js';
-
-
 const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
@@ -19,18 +12,7 @@ const {campgroundSchema,reviewSchema} = require('./schemas.js')
 
 require('dotenv').config();
 
-// const {createApi} = require('unsplash-js')
 
-
-// const unsplash = createApi({
-//     accessKey: '-rzil0-EoZN5be0YoX4LeViR-fJCnP0gdW4pDk5VrPU',
-//     fetch: nodeFetch,
-
-// });
-
-// const randomPhoto = unsplash.photos.getRandom({
-//     collectionIds: ['483251']
-// })
 
 const dbURI = process.env.dbURI
 mongoose.connect(dbURI)
