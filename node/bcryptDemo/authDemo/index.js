45ae6b53b7a61b876d84d9fea09f9d4b6af030e8
akosilar <55524614+akosilar@users.noreply.github.com>
2022-12-28 06:09:23 -0500

    added store username and password to db
    also includes hash and salt

--- node/bcryptDemo/authDemo/index.js
+++ node/bcryptDemo/authDemo/index.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const User = require('./models/user')
+const bcrypt = require('bcrypt')
+
+mongoose.connect('mongodb://localhost:27017/authDemo')
+.then(() => {
+    console.log('mongo connection open')
+})
+.catch(err => {
+    console.log('oh noes. Mongo connection problem')
+    console.log(err)
+})
 
 app.set('view engine', 'ejs')
 app.set('views', 'views')
 
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req,res) => {
+    res.send('this is home page')
+})
+
 app.get('/register', (req,res) => {
     res.render('register')
+})
+
+app.post('/register', async (req,res) => {
+    const {password, username} = req.body
+    const hash = await bcrypt.hash(password,12)
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save()
+    res.redirect('/')
 })
 
 app.get('/secret', (req,res) => {
