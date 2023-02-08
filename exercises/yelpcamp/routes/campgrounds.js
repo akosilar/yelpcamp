4b2f845fd0f0d249c26909698fa16a6bded2875b
akosilar <55524614+akosilar@users.noreply.github.com>
2023-02-08 01:13:01 -0500

    added permissions to  edit campground route
    only allows author to access the edit page

--- exercises/yelpcamp/routes/campgrounds.js
+++ exercises/yelpcamp/routes/campgrounds.js
@@ -59,12 +59,17 @@ router.get('/:id',catchAsync(async (req,res) => {
 
 //show edit campground
 router.get('/:id/edit',isLoggedIn,catchAsync(async(req,res) => {
-    const search = await Campground.findById(req.params.id)
+    const {id} = req.params
+    const search = await Campground.findById(id)
     if(!search) {
         req.flash('error', 'Cannot find that campground!')
         res.redirect('/campgrounds')
     }
     else {
+        if(!search.author.equals(req.user._id)){
+            req.flash('error', 'You do not have permission to do that!')
+            return res.redirect(`/campgrounds/${id}`)
+        }
         res.render('campgrounds/edit',{search})
 
     }
@@ -72,8 +77,14 @@ router.get('/:id/edit',isLoggedIn,catchAsync(async(req,res) => {
 
 //submit the campground edit to db
 router.put('/:id',isLoggedIn,validateCampground,catchAsync(async(req,res) => {
+
     const {id} = req.params
-    const update = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    const update = await Campground.findById(id)
+    if(!update.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that!')
+        return res.redirect(`/campgrounds/${id}`)
+    }
+    const camp = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${update._id}`)
 }))
