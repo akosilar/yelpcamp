d6c6eeb89bf5e6e37af97339de688ddc24207568
Lars Dev <janlarsaustero@gmail.com>
2023-02-14 20:46:51 -0500

    restructured users and campgrounds routes
    used router.route to group similarly named routes

--- exercises/yelpcamp/routes/campgrounds.js
+++ exercises/yelpcamp/routes/campgrounds.js
@@ -4,26 +4,43 @@ const catchAsync = require('../utils/catchAsync')
 const Campground = require('../models/campground')
 const {isLoggedIn,isAuthor,validateCampground} = require('../middleware');
 const campground = require('../models/campground');
-const campgrounds = require('../controllers/campgrounds')
+const campgrounds = require('../controllers/campgrounds');
+const { route } = require('./users');
+
+router.route('/')
+    .get(catchAsync(campgrounds.index)) //show a list of campgrounds
+    .post(isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground)) //add the new campground to db
 
 
-//show a list of campgrounds
-router.get('/', catchAsync(campgrounds.index))
 
 //show add new campground page
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground)) //show campground detail
+    .put(isLoggedIn,isAuthor,validateCampground,catchAsync(campgrounds.updateCampground)) //submit the campground edit to db
+    .delete(isLoggedIn,isAuthor,catchAsync(campgrounds.delete)) //delete the campground from db
+
+
+
+//show a list of campgrounds
+// router.get('/', catchAsync(campgrounds.index))
+
+
+
 //add the new campground to db
-router.post('/', isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground))
+// router.post('/', isLoggedIn,validateCampground,catchAsync(campgrounds.createCampground))
 
 //show campground detail
-router.get('/:id',catchAsync(campgrounds.showCampground))
+// router.get('/:id',catchAsync(campgrounds.showCampground))
 
 //show edit campground
 router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
 //submit the campground edit to db
-router.put('/:id',isLoggedIn,isAuthor,validateCampground,catchAsync(campgrounds.updateCampground))
+// router.put('/:id',isLoggedIn,isAuthor,validateCampground,catchAsync(campgrounds.updateCampground))
 
 //delete the campground from db
-router.delete('/:id', isLoggedIn,isAuthor,catchAsync(campgrounds.delete))
+// router.delete('/:id', isLoggedIn,isAuthor,catchAsync(campgrounds.delete))
+
 module.exports = router
--- exercises/yelpcamp/routes/users.js
+++ exercises/yelpcamp/routes/users.js
@@ -5,20 +5,23 @@ const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
 const users = require('../controllers/users')
 
-router.get('/register', users.renderRegister)
 
-router.post('/register',catchAsync(users.register))
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
 
-router.get('/login', users.renderLogin)
 
-router.post('/login', passport.authenticate('local',{failureFlash: true, failureRedirect: '/login',keepSessionInfo:true}), users.login)
-
-router.get('/logout', (req,res) => {
-    req.logout(function(err) {
-        if(err) {return next(err)}
-        req.flash('success', 'goodbye!')
-        res.redirect('/campgrounds')
-    })
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local',{failureFlash: true, failureRedirect: '/login',keepSessionInfo:true}), users.login)
     
-})
+// router.get('/register', users.renderRegister)
+
+// router.post('/register',catchAsync(users.register))
+
+// router.get('/login', users.renderLogin)
+
+// router.post('/login', passport.authenticate('local',{failureFlash: true, failureRedirect: '/login',keepSessionInfo:true}), users.login)
+
+router.get('/logout', users.logout)
 module.exports = router;
