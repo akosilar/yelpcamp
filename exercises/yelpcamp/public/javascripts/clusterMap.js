7d43b4e5829145ccfcb4b71cec761e8a488afbbb
akosilar <55524614+akosilar@users.noreply.github.com>
2023-03-19 20:13:07 -0400

    modified custom popup to show campground link

--- exercises/yelpcamp/public/javascripts/clusterMap.js
+++ exercises/yelpcamp/public/javascripts/clusterMap.js
@@ -7,6 +7,8 @@ const map = new mapboxgl.Map({
     zoom: 3
 });
 
+
+
 map.on('load', () => {
     // Add a new source from our GeoJSON data and
     // set the 'cluster' option to true. GL-JS will
@@ -102,10 +104,11 @@ map.on('load', () => {
     // the location of the feature, with
     // description HTML from its properties.
     map.on('click', 'unclustered-point', (e) => {
+        const { popUpMarkup } = e.features[0].properties
         const coordinates = e.features[0].geometry.coordinates.slice();
-        const mag = e.features[0].properties.mag;
-        const tsunami =
-            e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
+        // const mag = e.features[0].properties.mag;
+        // const tsunami =
+        //     e.features[0].properties.tsunami === 1 ? 'yes' : 'no';
 
         // Ensure that if the map is zoomed out such that
         // multiple copies of the feature are visible, the
@@ -117,7 +120,8 @@ map.on('load', () => {
         new mapboxgl.Popup()
             .setLngLat(coordinates)
             .setHTML(
-                `magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`
+                // `magnitude: ${mag}<br>Was there a tsunami?: ${tsunami}`
+                `${popUpMarkup}`
             )
             .addTo(map);
     });
